fa2c88a35e537359c4c6450b8f778787916d971d
MPThLee <mpthlee@gmail.com>
2018-05-11 17:34:22 +0900

    Fix build test error for checking.
    It needs exit code is not 0.

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,11 @@
 import check_hashes from "./check";
 
+async function process_exit() {
+  setTimeout(function() {
+    process.exit(1);
+  }, 1000);
+}
+
 check_hashes()
   .then(item => {
     if (
@@ -11,13 +17,15 @@ check_hashes()
         "Test is unsuccessful.\n" +
         "Maybe hashes are changed or Forbidden by AWS/enza\n" +
         "Received items:\n" +
-        item;
+        item +
+        "\n";
       throw new Error(errstr);
     } else {
-      console.log("Test Suceess.")
+      console.log("Test Suceess.");
     }
   })
   .catch(err => {
-    const errstr = "Test Failed.\n" + "Error:\n" + err;
+    const errstr = "Test Failed.\n" + "Error:\n" + err.stack;
+    process_exit();
     throw new Error(errstr);
   });
