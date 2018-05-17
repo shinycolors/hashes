409aa4aa15281d0da87974cfe0ad51e2aaabdab2
MPThLee <mpthlee@gmail.com>
2018-05-17 17:39:13 +0900

    Update build script.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
-var gulp = require("gulp");
-var ts = require("gulp-typescript");
-var tsProject = ts.createProject("tsconfig.json");
+const del = require("del");
+const gulp = require("gulp");
+const sequence = require("gulp-sequence");
+const ts = require("gulp-typescript");
+const tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("default", function() {
+gulp.task("build", function() {
   return tsProject
     .src()
     .pipe(tsProject())
     .js.pipe(gulp.dest("dist"));
 });
+
+gulp.task("clean", function() {
+  return del(['dist/**/*', '!dist/.gitkeep']);
+});
+
+gulp.task("watch", function() {
+  gulp.watch("src/**/*", ["build"]);
+});
+
+gulp.task("default", sequence('clean', 'build'));
